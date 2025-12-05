505ea19cb64f754057ef848cc650a8b648180f8e
jetcouper <Yoshi9603!?Git>
2025-12-05 11:14:26 -0500

    TP3

--- TP3/app/pages/pageCoordonnee.js
+++ TP3/app/pages/pageCoordonnee.js
@@ -1,10 +1,15 @@
+import Slider from '@react-native-community/slider';
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-export default function pageCoordonnee() {
+import { commerces } from '../../assets/libs/donnees';
+
+const pageCoordonnee = () => {
     const [location, setLocation] = useState(null);
     const [errorMsg, setErrorMsg] = useState("");
+    const [valeurSlide, setValeurSlide] = useState(0.00)
+    const [filteredData, setFilteredData] = useState(commerces);
 
     useEffect(() => {
         async function getCurrentLocation() {
@@ -28,6 +33,18 @@ export default function pageCoordonnee() {
         getCurrentLocation();
     }, []);
 
+    const renderListItem = ({ item }) => (
+        <Text style={styles.titleText}>{item.nom}</Text>
+
+    );
+    const DATA = [
+        { id: '1', nom: 'Item 1' },
+        { id: '2', nom: 'Item 2' },
+        { id: '3', nom: 'Item 3' },
+    ];
+
+
+
     let text = 'En attente...';
     if (errorMsg) {
         text = errorMsg;
@@ -45,6 +62,28 @@ export default function pageCoordonnee() {
                 <>
                     <Text>Latitude: {location.coords.latitude}</Text>
                     <Text>Longitude: {location.coords.longitude}</Text>
+                    <View style={styles.slider}>
+                        <Slider
+                            style={{ width: 200, height: 40 }}
+                            minimumValue={0}
+                            maximumValue={3000}
+                            minimumTrackTintColor="#FFFFFF"
+                            maximumTrackTintColor="#000000"
+                            onValueChange={(value) => setValeurSlide(value)}
+
+                        />
+                        <Text style={styles.textSlider}>
+                            {Math.round(valeurSlide)}
+                        </Text>
+                    </View>
+                    <FlatList
+                        style={styles.flat}
+                        data={commerces}
+                        renderItem={renderListItem}
+                        keyExtractor={(item) => item.id.toString()}
+                        ItemSeparatorComponent={<View style={styles.separateur}></View>}
+                    />
+
                 </>
             ) : (
                 <Text>Chargement...</Text>
@@ -57,13 +96,33 @@ const styles = StyleSheet.create(
     {
         main: {
             alignItems: 'center',
-            justifyContent: 'center',
             flex: 1,
             gap: 10,
+            paddingTop: 20,
         },
         text: {
             fontSize: 20,
             fontWeight: 'bold'
         },
+        slider: {
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height:100
+            //flex: 1,
+        },
+        textSlider: {
+            width: 70,
+            textAlign: 'center'
+        },
+        flat: {
+            width: '100%',
+            //backgroundColor: '#ADD8E6',
+        },
+        titleText: {
+            fontSize: 24,
+        }
     }
 )
+
+export default pageCoordonnee;
